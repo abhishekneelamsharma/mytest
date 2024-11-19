749a6b09dfe9730f6166360bb5ccae30ea90e6a6
Abhishek <abhisheksharma0573@gmail.com>
2024-11-19 16:51:21 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
 
   const handleTouchStart = (event) => {
     const touch = event.touches[0];
-    
-    if (touch.clientX < window.innerWidth * 0.1 || touch.clientX > window.innerWidth * 0.9) {
+
+    if (touch.clientX > window.innerWidth * 0.1 && touch.clientX < window.innerWidth * 0.9) {
       event.preventDefault();
+  
     }
-    if (touch.clientY > window.innerHeight * 0.9) {
+    if (touch.clientY < window.innerHeight * 0.9) {
       event.preventDefault();
+      
     }
   };
 
   useEffect(() => {
-
     document.addEventListener("touchstart", handleTouchStart, { passive: false });
-
     return () => {
       document.removeEventListener("touchstart", handleTouchStart,{ passive: false });
     };
   }, [])
+
+
+  const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const disableButton = () => {
+  //     window.history.pushState(null,'',window.location.href);
+  //     window.onpopstate = function(event){
+  //       window.history.go(1)
+  //     }
+  //   }
+  //   disableButton();
+  //   return ()=>{
+  //     window.onpopstate = null
+  //   }
+  // }, [])
+
 
   return (
     <div className="App">
